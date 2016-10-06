f49f8b7c26697e28c12e73340a5ece6e6963d7ba
hotamateurxxx <hotamateurxxx@gmail.com>
2016-10-06 16:12:02 +0400

    Добавил проект для тестирования особенностей наследования.

---
+++ InheritanceTests/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTests
+{
+
+    class A
+    {
+
+        public A()
+        {
+        }
+
+        public A(string param) 
+            : this()
+        {
+        }
+
+    }
+
+    class B : A
+    {
+
+        public B(string param) 
+            : base(param)
+        {
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new B("paramValue");
+        }
+    }
+
+}
