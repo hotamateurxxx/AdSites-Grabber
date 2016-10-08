2889452e09d052e5728db0700a3f047396c9d006
hotamateurxxx <hotamateurxxx@gmail.com>
2016-10-08 04:58:19 +0400

    Добавил простейший рабочий пример с использованием NHibernate.

---
+++ NHibernateTests/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace NHibernateTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ISessionFactory sessionFactory = new Configuration().Configure().BuildSessionFactory();
+
+            ISession currentSession = sessionFactory.OpenSession();
+
+            Item item = new Item();
+            item.Name = "Item1";
+
+            ITransaction tx = currentSession.BeginTransaction();
+            currentSession.Save(item);
+            tx.Commit();
+
+            currentSession.Close();
+
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+
+        }
+    }
+}
