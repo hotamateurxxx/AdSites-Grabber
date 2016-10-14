a5c2834547756af4b5b7c4a00d5ddc0012e62da8
hotamateurxxx <hotamateurxxx@gmail.com>
2016-10-14 18:22:50 +0400

    Добавил проект для тестирования log4net.

---
+++ Log4Net Tests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using log4net;
+using log4net.Config;
+
+namespace Log4Net_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+
+            ILog log = LogManager.GetLogger("InfoLogger");
+            log.Info("Starting application.");
+
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+
+        }
+    }
+}
