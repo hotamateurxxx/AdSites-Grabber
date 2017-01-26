423cb84a74264fcb491226e7045a1932c480e699
hotamateurxxx <hotamateurxxx@gmail.com>
2017-01-27 00:37:35 +0400

    Добавил беглые ожидания.

--- Grabber/Controller/AvitoGrabber.cs
+++ Grabber/Controller/AvitoGrabber.cs
@@ -49,7 +49,6 @@ namespace AdSitesGrabber.Controller
                 driver.Navigate().GoToUrl(Url);
                 // Выбираем город
                 selectLocation(driver);
-                Thread.Sleep(1000);
                 // Обрабатываем объявления на текущей странице
                 processPageAdverts(driver);
                 // Когда закончили читать объявления в списках - заходим по ссылке на каждое объявление и дочитываем его
@@ -79,7 +78,8 @@ namespace AdSitesGrabber.Controller
         {
             try
             {
-                IWebElement link = driver.FindElement(By.LinkText(Region));
+                IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+                IWebElement link = wait.Until(drv => drv.FindElement(By.LinkText(Region)));
                 link.Click();
             }
             catch (NoSuchElementException e)
@@ -96,7 +96,8 @@ namespace AdSitesGrabber.Controller
         protected void processPageAdverts(IWebDriver driver)
         {
             System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> divs;
-            divs = driver.FindElements(By.CssSelector(".catalog-list div.item_table"));
+            IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            divs = wait.Until(drv => drv.FindElements(By.CssSelector(".catalog-list div.item_table"));
             if (divs.Count == 0)
                 throw new Exception("На странице не найдено объявлений.");
             foreach (IWebElement div in divs)
