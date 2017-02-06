0b4f8b40aa076d9bc10aa6ef8ac52e50f7e0b966
hotamateurxxx <hotamateurxxx@gmail.com>
2017-02-07 02:00:10 +0400

    Довел до рабочего состояния тестовое приложение.

--- src/Tests/AvitoParsing/AdvertPage/Program.cs
+++ src/Tests/AvitoParsing/AdvertPage/Program.cs
@@ -1,4 +1,9 @@
-﻿using AdSitesGrabber.Extensions;
+﻿using AdSitesGrabber.Controller;
+using AdSitesGrabber.Controller.Avito;
+using AdSitesGrabber.Extensions;
+using AdSitesGrabber.Model;
+using log4net.Config;
+using OpenQA.Selenium;
 using System;
 
 namespace AdvertPage
@@ -21,13 +26,38 @@ namespace AdvertPage
             var options = CommandLine.Parser.Default.ParseArguments<CommandLineArguments>(args);
             IWebDriverExtension.WaitTimeout = options.Value.WaitTimeout;
 
+            try
+            {
+                // Запускаем конфигурацию log4net напрямую, потому что непонятно что будет отрабатывать раньше:
+                // мой вызов LogManager или какая-то из инструкций NHibernate.
+                XmlConfigurator.Configure();
 
+                using (WebManager webManager = WebManager.GetInstance())
+                {
 
+                    // Конфигурация веб-менеджера
+                    webManager.FactoryDriver = AdSitesGrabber.Controller.WebManager.DriverType.Firefox;
+                    webManager.BrowserPath = options.Value.BrowserPath;
+
+                    IWebDriver driver = webManager.OccupyDriver(webManager);
+                    var parser = new AvitoAdvertOnPageParser(driver);
+                    Advert advert = parser.Parse(options.Value.Url);
+                    webManager.ReleaseDriver(driver);
+                    Console.WriteLine("Со страницы прочитано следущее объявление:\n" + advert);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Warns.Error("Ошибка выполнения программы.", e);
+            }
+            
             Console.WriteLine();
             Console.WriteLine("Программа выполнена. Для выхода нажмите Enter.");
             Console.ReadLine();
             Console.WriteLine("Здесь должно все закончиться.");
 
         }
+
     }
 }
